e152725fc8ccd86385bd8b1b15f80f3708c9c3b5
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2013-10-14 20:01:32 -0300

    xrandr commands for multiple monitors

---
+++ xrandr/benq.sh
@@ -0,0 +1,1 @@
+xrandr --output VGA1 --mode "1280x720" --above LVDS1
---
+++ xrandr/ilo.sh
@@ -0,0 +1,6 @@
+xrandr --newmode "1368x768_60.00"  85.86  1368 1440 1584 1800  768 769 772 795  -HSync +Vsync
+
+xrandr --addmode VGA1 "1368x768_60.00"
+
+xrandr --output VGA1 --mode "1368x768_60.00"
+
